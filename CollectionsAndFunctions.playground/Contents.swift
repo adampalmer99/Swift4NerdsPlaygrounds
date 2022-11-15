1a721ded8f2c524d3b6a76eb0d562172cec1c859
Adam Palmer <acpalmer1011@gmail.com>
2022-11-15 14:49:37 +0000

    Finished Arrays in CollectionsAndFunctions

--- CollectionsAndFunctions.playground/Contents.swift
+++ CollectionsAndFunctions.playground/Contents.swift
@@ -25,3 +25,83 @@ print(bucketList[0...2])
 // Subscripting to append new info
 bucketList[2] += " in Australia"
 print(bucketList)
+
+// replacing an Array item
+bucketList[0] = "Climb mt. Everest"
+bucketList
+
+
+var secondBucketList = ["Ski in Canada"]
+
+var newItems = [
+"Fly hot air balloon",
+"Watch LOTR in one day",
+"Visit Los Angeles",
+"Visit a Scandinavian country",
+"Win the lottery"
+]
+
+// Using a loop to append items from one array to another
+
+//for item in newItems {
+//    secondBucketList.append(item)
+//}
+print(secondBucketList)
+
+// Refactoring with the addition and assignment operator +=
+
+secondBucketList += newItems
+print(secondBucketList)
+
+// Inserting a new ambition
+
+secondBucketList.insert("Visit the pyramids", at: 1)
+print(secondBucketList)
+
+//MARK: - Array Equality
+
+var terrysList = [
+"Ski in Canada",
+"Win the lottery",
+"Visit the pyramids",
+"Fly hot air balloon",
+"Visit a Scandinavian country",
+"Watch LOTR in one day",
+"Visit Los Angeles"
+]
+
+let equal = (secondBucketList == terrysList)
+// False because the order is different. ARRAYS ARE ORDERED!!
+
+//MARK: - Immutable Arrays
+
+let lunches = [
+"Cheeseburger",
+"Veggie Pizza",
+"Chicken Caesar Salad",
+"Black Bean Burrito",
+"Falafel Wrap"
+]
+// LET keyword creates the immutable array
+// CANNOT MUTATE AN IMMUTABLE ARRAY
+
+//MARK: - Bronze Challenge page 94
+// Use the documentation to locate a var defined on the array type that will tell you whether toDoList contains any elements
+
+var toDoList = ["Take out garbage", "Pay bills", "Cross off finished items"]
+print(toDoList.isEmpty)
+
+//MARK: - silver challenge
+// Use a loop to reverse the order of the elements in toDoList and log results to console, and find easier way to do it
+
+var secondToDoList = [""]
+
+for items in toDoList {
+    secondToDoList.append(items)
+    secondToDoList.reversed()
+    print(secondToDoList)
+}
+
+
+toDoList.reverse()
+print(toDoList)
