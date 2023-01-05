d2184e15400cd5770c825dfbb2f4d9e223b4c24b
Adam Palmer <acpalmer1011@gmail.com>
2023-01-05 15:20:41 +0000

    Aligning columns, formatting a table of data

--- Protocols.playground/Contents.swift
+++ Protocols.playground/Contents.swift
@@ -1,14 +1,34 @@
 import Cocoa
 
-func printTable(_ data: [[String]]) {
+func printTable(_ data: [[String]], withColumnLabels columnLabels: String...) {
+    // Create first row containing column headers
+    var firstRow = "|"
+    
+    // Keep track of column width
+    var columnWidths = [Int]()
+    
+    for columnLabel in columnLabels {
+        let columnHeader = " \(columnLabel) |"
+        firstRow += columnHeader
+        columnWidths.append(columnLabel.count)
+    }
+    print(firstRow)
+    
     for row in data {
         // Start the output string
         var out = "|"
         
         // Append each item in this row to the string
-        for item in row {
-            out += " \(item) |"
+//        for item in row {
+//            out += " \(item) |"
+//        }
+        // Append each item in this row to the String
+        for (j, item) in row.enumerated() {
+            let paddingNeeded = columnWidths[j] - item.count
+            let padding = repeatElement(" ", count: paddingNeeded).joined(separator: "")
+            out += " \(padding)\(item) |"
         }
+        
         
         // Done- print it
         print(out)
@@ -20,4 +40,4 @@ let data = [
 ]
 
 
-printTable(data)
+printTable(data, withColumnLabels: "Employee Name", "Age", "Years of experience")
