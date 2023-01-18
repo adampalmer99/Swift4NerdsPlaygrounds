419ece0092d39b96dba6eba96f7828d9590ff4e2
Adam Palmer <acpalmer1011@gmail.com>
2023-01-18 15:40:40 +0000

    Protocols extensions

--- ProtocolExtensions.playground/Contents.swift
+++ ProtocolExtensions.playground/Contents.swift
@@ -1,13 +1,21 @@
 import Cocoa
 
-protocol Exercise {
+protocol Exercise: CustomStringConvertible {
     var name: String { get }
     var caloriesBurned: Double { get }
     var minutes: Double { get }
 }
 
+// Adding default implementation of description to Exercise. Types that conform to Exercise don't have to provide it themselves.
+extension Exercise {
+    var description: String {
+        return "Exercise (\(name), burned \(caloriesBurned) calories in \(minutes) minutes)"
+    }
+}
+
 struct EllipticalWorkout: Exercise {
     let name = "Elliptical Workout"
+    let title = "Workout using the Go Fast Elliptical Trainer"
     let caloriesBurned: Double
     let minutes: Double
 }
@@ -18,9 +26,16 @@ struct TreadmillWorkout: Exercise {
     let name = "Treadmill Workout"
     let caloriesBurned: Double
     let minutes: Double
+    let laps: Double
 }
 
-let runningWorkout = TreadmillWorkout(caloriesBurned: 350, minutes: 25)
+extension TreadmillWorkout {
+    var description: String {
+        return "TreadmillWorkout (\(caloriesBurned) calories and \(laps) laps in \(minutes) minutes)"
+    }
+}
+
+let runningWorkout = TreadmillWorkout(caloriesBurned: 350, minutes: 25, laps: 10.5)
 
 //MARK: - Extending Exercise
 
@@ -38,6 +53,8 @@ extension Exercise {
     }
 }
 
+
+
 extension Sequence where Iterator.Element == Exercise {
     func totalCaloriesBurned() -> Double {
         var total: Double = 0
@@ -50,3 +67,18 @@ extension Sequence where Iterator.Element == Exercise {
 
 let mondayWorkout: [Exercise] = [ellipticalWorkout, runningWorkout]
 print(mondayWorkout.totalCaloriesBurned())
+
+print(ellipticalWorkout)
+print(runningWorkout)
+print(ellipticalWorkout.title)
+
+//MARK: - Extending Exercise to add a title
+extension Exercise {
+    var title: String {
+        return "\(name) - \(minutes) minutes"
+    }
+}
+
+for exercise in mondayWorkout {
+    print(exercise.title)
+}
